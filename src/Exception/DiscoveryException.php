5b04a01deb7f54881946cffc773fbb8e3164f03b
Andreas Hennings <andreas@dqxtech.net>
2024-05-10 23:00:13 +0200

    Move some exception classes to class-discovery package.

---
+++ src/Exception/DiscoveryException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Donquixote\ClassDiscovery\Exception;
+
+/**
+ * Something went wrong during discovery.
+ */
+class DiscoveryException extends \Exception {
+
+}
